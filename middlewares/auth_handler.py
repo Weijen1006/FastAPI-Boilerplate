a50aaa60d793965c969cbb074cd8b35f23dbeb84
Thing Wei Jen <weijen06@hotmail.com>
2025-06-01 22:27:12 +0800

    fix: removed async auth handler and combined into one function

--- middlewares/auth_handler.py
+++ middlewares/auth_handler.py
@@ -4,12 +4,13 @@ from models.user import UserData
 from functools import wraps
 from typing import Callable
 from auth.auth_handler import has_permission
+import asyncio
 
 
 def with_role(required_role: str):
     def decorator(func: Callable):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
+
+        def check_permission(kwargs):
             current_user: UserData = kwargs.get('current_user')
             if not current_user:
                 raise HTTPException(status_code=403, detail=f"User undefined")
@@ -18,24 +19,19 @@ def with_role(required_role: str):
 
             if not has_permission(current_user.role, required_role):
                 raise HTTPException(status_code=403, detail=f"Missing permission for: {required_role}")
-            
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
 
-def async_with_role(required_role: str):
-    def decorator(func: Callable):
         @wraps(func)
-        async def wrapper(*args, **kwargs):
-            current_user: UserData = kwargs.get('current_user')
-            if not current_user:
-                raise HTTPException(status_code=403, detail=f"User undefined")
-            elif current_user.role not in [role for role in ROLE_HIERARCHY]:
-                raise HTTPException(status_code=403, detail=f"Invalid role: {current_user.role}")
-
-            if not has_permission(current_user.role, required_role):
-                raise HTTPException(status_code=403, detail=f"Missing permission for: {required_role}")
-            
+        async def async_wrapper(*args, **kwargs):
+            check_permission(kwargs)
             return await func(*args, **kwargs)
-        return wrapper
+
+        @wraps(func)
+        def sync_wrapper(*args, **kwargs):
+            check_permission(kwargs)
+            return func(*args, **kwargs)
+
+        if asyncio.iscoroutinefunction(func):
+            return async_wrapper
+        else:
+            return sync_wrapper
     return decorator
